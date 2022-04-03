bdc930ce75f0ebafd85246da2efd91f8af806b0e
ashley68 <ashley0608ly@gmail.com>
2022-04-03 15:39:44 -0400

    component modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,77 @@
 import React from "react";
 import "./style.css";
 import Button from "./Button";
+// import { createUseStyles } from "react-jss";
+
+//css-in-js
+// const useStyles = createUseStyles({
+//   container: {
+//     display: "grid",
+//     gridtTemplateColumns: "repeat(2, 1fr)",
+//     gridTemplateRows: "repeat(5, 1fr)",
+//     width: "20%",
+//     border: "1px solid black",
+//   },
+// });
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { capacity: 0, message: "" };
+    this.state = { capacity: 99, message: "" };
   }
 
-  display = (updatedCapacity) => {
-    this.setState({ capacity: updatedCapacity });
-    if (updatedCapacity >= 100) {
-      this.setState({ message: "Max capacity is reached!" });
-    } else {
-      this.setState({ message: "" });
-    }
-  };
+  // classes = useStyles();
+
+  // display = (updatedCapacity) => {
+  //   this.setState({ capacity: updatedCapacity });
+  //   if (updatedCapacity >= 100) {
+  //     this.setState({ message: "Max capacity is reached!" });
+  //   } else {
+  //     this.setState({ message: "" });
+  //   }
+  // };
 
   reset = () => {
     this.setState({ capacity: 0, message: "" });
+  };
+
+  increBtnClicked = () => {
+    if (this.state.capacity < 100) {
+      this.setState({ capacity: this.state.capacity + 1 });
+    }
+  };
+
+  decreBtnClicked = () => {
+    if (this.state.capacity > 0) {
+      this.setState({ capacity: this.state.capacity - 1 });
+    }
   };
 
   render() {
     return (
       <div className="container">
         <div className="div1">
-          <button className="reset" onClick={this.reset}>
-            Reset
-          </button>
+          <Button classes="reset" onClick={this.reset} label="reset"></Button>
           <div className="item capacity-label">
             <h2>Max Capacity:100</h2>
             <h1 className="display">{this.state.capacity}</h1>
-            <h2>{this.state.message}</h2>
+            <h2>
+              {this.state.capacity === 100 ? "Max capacity is reached!" : ""}
+            </h2>
           </div>
         </div>
         <div className="item div2">
           <Button
-            clicked={this.display}
-            capacity={this.state.capacity}
-            label={"-"}
+            classes="counterBtn"
+            onClick={this.decreBtnClicked}
+            label="-"
           />
         </div>
         <div className="item div3">
           <Button
-            clicked={this.display}
-            capacity={this.state.capacity}
-            label={"+"}
+            classes="counterBtn"
+            onClick={this.increBtnClicked}
+            label="+"
           />
         </div>
         <div className="div4"></div>
--- src/Button.jsx
+++ src/Button.jsx
@@ -1,25 +1,10 @@
 import React from "react";
-import "./style.css";
 
 class Button extends React.Component {
-  increBtnClicked = () => {
-    if (this.props.capacity < 100) {
-      this.props.clicked(this.props.capacity + 1);
-    }
-  };
-  decreBtnClicked = () => {
-    if (this.props.capacity > 0) {
-      this.props.clicked(this.props.capacity - 1);
-    }
-  };
-
-  handleClick =
-    this.props.label === "+" ? this.increBtnClicked : this.decreBtnClicked;
-
   render() {
     return (
       <div>
-        <button className="counterBtn" onClick={this.handleClick}>
+        <button className={this.props.classes} onClick={this.props.onClick}>
           {this.props.label}
         </button>
       </div>
